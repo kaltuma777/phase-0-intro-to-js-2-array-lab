49b2c3552401153b1f56a4581d183c2615981281
Kaltuma777 <Kaltuma.abdi@student.moringaschool.com>
2023-10-26 21:57:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+
+  function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+  }
+  function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+  }
+  function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+  }
+ 
+  module.exports = {
+    appendCat,
+    cats,
+  };
+
+
+function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;
+  }
+ 
+  module.exports = {
+    prependCat,
+    cats,
+  };
+
+  function removeLastCat() {
+    if (cats.length === 0) {
+      return cats;
+    }
+    const newCats = [...cats];
+    newCats.pop();
+    return newCats;
+  }
+ 
+  module.exports = {
+    removeLastCat,
+    cats,
+  };
+
+  function removeFirstCat() {
+    if (cats.length === 0) {
+      return cats;
+    }
+    const newCats = [...cats];
+    newCats.shift();
+    return newCats;
+  }
+ 
+  module.exports = {
+    removeFirstCat,
+    cats,
+  };
